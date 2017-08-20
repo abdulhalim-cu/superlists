f0e58b5231cffc2072467bff0bb34e4626f2a03a
Abdul Halim <abdulhalim.cu10@gmail.com>
2017-08-20 06:58:51 +0600

    Refactoring our FT and some change in home.html

--- lists/tests.py
+++ lists/tests.py
@@ -11,7 +11,7 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
 
-
+	
 	def test_home_page_returns_correct_html(self):
 		# Before 
 		# response = home_page(request)
@@ -44,4 +44,15 @@ class HomePageTest(TestCase):
 		# [ assertTemplateUsed ] is the test method that the django TestCase class
 		# provides us. It lets us check what template was used to render a response
 		# (NB - it will only work for responses that were retrieved by the Test Client.)
+		self.assertTemplateUsed(response, 'home.html')
+	
+
+	def test_uses_home_template(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
+
+
+	def test_can_save_a_post_request(self):
+		response = self.client.post('/', data={'item_text':'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
