0a647bce0db92b310f89fb261b403598ef0e8535
abdul halim <abdulhalim.cu10@gmail.com>
2017-08-23 19:50:21 +0600

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -1,78 +1,61 @@
-from django.urls import resolve
-from django.http import HttpRequest
-from django.test import TestCase
-from lists.views import home_page
-from django.template.loader import render_to_string # render the template in test
 from lists.models import Item
+from django.template.loader import render_to_string
+from django.urls import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
+
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+    def test_uses_home_template(self):
+        # Django Test Client has built-in ways of checking what templates
+        #  are used.
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-	
-	def test_home_page_returns_correct_html(self):
-		# Before 
-		# response = home_page(request)
-		
-		# Now
-		# [self.client.get('/')] checks which template is used for root 
-		# Instead of manually creating an HttpRequest object and calling the view
-		# function directly, we call self.client.get , passing it the URL we want
-		# to test
-		response = self.client.get('/')
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
 
-		# Use [request = HttpRequest()] only if [response = home_page(request)] is used
-		# request = HttpRequest()
-		# docode is also not import if we use client.get method. So lets comment out this line
-		# html = response.content.decode('utf-8')
-		
-		# Use these three lines of code that given below. Finally we are not going to 
-		# use these three lines of code. So we comment out 
-		# [start]
-		# self.assertTrue(html.strip().startswith('<html>'))
-		# self.assertIn('<title>To-Do lists</title>', html)
-		# self.assertTrue(html.strip().endswith('</html>'))
-		# [end]
-		
-		# or use next two lines of code [start]
-		# expected_html = render_to_string('home.html')
-		# self.assertEqual(html, expected_html)
-		# [end]
-		
-		# [ assertTemplateUsed ] is the test method that the django TestCase class
-		# provides us. It lets us check what template was used to render a response
-		# (NB - it will only work for responses that were retrieved by the Test Client.)
-		self.assertTemplateUsed(response, 'home.html')
-	
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
-	def test_uses_home_template(self):
-		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'home.html')
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
 
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
-	def test_can_save_a_post_request(self):
-		response = self.client.post('/', data={'item_text':'A new list item'})
-		self.assertIn('A new list item', response.content.decode())
-		self.assertTemplateUsed(response, 'home.html')
+    def test_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
 
-	def test_saving_and_retrieving_items(self):
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.save()
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
 
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.save()
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
 
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
 
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(second_saved_item.text, 'Item the second')
-
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
